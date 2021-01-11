459bfe529eaffc19c6013671df0c9ee3201a8a98
Sayem Kader Chowdhury <kaderchowdhury.sayem@gmail.com>
2021-01-11 23:03:05 +0600

    added pagination support

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
+from django.core import paginator
 from django.shortcuts import render, get_object_or_404
 from . models import Post
 
+# This import is for the pagination for the posts
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+# This import is for the class based view for pagination
+from django.views.generic import ListView
+
+
+# post_list & post_detail is about how the blog will appear and
+# seo friendly url
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -16,3 +36,10 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
