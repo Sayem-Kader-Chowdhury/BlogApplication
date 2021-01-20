e49aa379b96c7fedf91491178f713cc240c4628f
Sayem Kader Chowdhury <kaderchowdhury.sayem@gmail.com>
2021-01-20 17:52:51 +0600

    Added sitemap to the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
