769f162a8526ada2e1db886b15fd37710a556025
Sayem Kader Chowdhury <kaderchowdhury.sayem@gmail.com>
2021-01-21 21:35:22 +0600

    Stemming and ranking results

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ from taggit.models import Tag
 from django.db.models import Count, query
 
 # These are added for the search functionaily against multiple fields
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # post_list & post_detail is about how the blog will appear and
@@ -133,8 +133,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     return render(request, 'blog/post/search.html',
                   {'form': form,
                    'query': query,
